f0ce05209d58cb4952d6f6d511696d889de62063
Federico Tudini <fede.tudini98@gmail.com>
2024-06-06 19:36:18 +0200

    creazione chips e cards pronostici

--- src/app/components/predictions/predictions.component.ts
+++ src/app/components/predictions/predictions.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatchesService } from '../../services/matches.service';
 
 @Component({
   selector: 'app-predictions',
   templateUrl: './predictions.component.html',
   styleUrl: './predictions.component.scss'
 })
-export class PredictionsComponent {
+export class PredictionsComponent implements OnInit {
+
+  public matches!: any[];
+
+  constructor(private matchesService: MatchesService) {
+
+  }
+
+  ngOnInit() {
+    this.loadData()
+  }
+
+  public loadData() {
+    this.matchesService.getMatches().subscribe(
+      (data : any) => {
+        this.matches = data.matches
+       // this.groupItemsByDate();
+      }
+    )
+  }
 
 }
